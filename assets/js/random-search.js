6be0d0e574352bace1e64817e222146789f77087
Jude <storeyjude@hotmail.com>
2020-03-11 17:21:45 +0000

    changing from fetch to xml request

--- assets/js/random-search.js
+++ assets/js/random-search.js
@@ -1,7 +1,20 @@
-fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+const baseURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
