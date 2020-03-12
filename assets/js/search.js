64d993f9fd18a9437cdb7f92fd9f91fc7ec5c5a4
Jude <storeyjude@hotmail.com>
2020-03-12 17:56:10 +0000

    tidying up code

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,7 +1,32 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+const baseURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function writeToDocument(type) {
+
+    getData(type, function (data) {
+        //div with id=data printing this//
+        document.getElementById("recipe-title").innerHTML = data.meals["0"].strMeal;
+        //food category breakfast lunch diner desert etc
+        document.getElementById("recipe-category").innerHTML = data.meals["0"].strCategory;
+        //link needs converted to image
+        document.getElementById("recipe-image").innerHTML = data.meals["0"].strMealThumb;
+        //need list of ingredients an measures
+
+        // description
+        document.getElementById("recipe-instruction").innerHTML = data.meals["0"].strInstructions;        
+        //youtube clip needs embeeded
+        document.getElementById("recipe-vid").innerHTML = data.meals["0"].strYoutube;
+    });
+}
