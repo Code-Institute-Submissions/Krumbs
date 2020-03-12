8464b4368e882202277def7a35b347df71477973
Jude <storeyjude@hotmail.com>
2020-03-12 20:31:30 +0000

    tried adding img and vid link from json object but currently broken

--- assets/js/search.js
+++ assets/js/search.js
@@ -20,20 +20,23 @@ function writeToDocument(type) {
         document.getElementById("recipe-title").innerHTML = data.meals["0"].strMeal;
         //food category breakfast lunch diner desert etc
         document.getElementById("recipe-category").innerHTML = data.meals["0"].strCategory;
-        //link needs converted to image instead of showing a website link
-        document.getElementById("recipe-image").innerHTML = data.meals["0"].strMealThumb;
+
+         //link needs converted to image instead of showing a website link/ or broken image
+        document.getElementById("recipe-image").innerHTML = '<img src="data.meals["0"].strMealThumb">';
+
+       //document.getElementById("recipe-image").innerHTML = '<img src="data.meals["0"].strMealThumb">';
         //need list of ingredients an measures 1-20 on a for loop? Unpacking Our Data Onto The DOM lesson: 
         //checks if the meal has that corresponding ingredient-measure pair. If it does, we're putting it into the ingredients array. 
         //If there aren't any more ingredients we're stopping the for loop with a break condition. needs figured out next.
 
 
-
-
-
         
         // description of how to carry out the meal
         document.getElementById("recipe-instruction").innerHTML = data.meals["0"].strInstructions;        
-        //youtube clip needs embeeded as video instead of website link
-        document.getElementById("recipe-vid").innerHTML = data.meals["0"].strYoutube;
+        //youtube clip needs embeeded as video instead of website link/ broken link
+        document.getElementById("recipe-vid").innerHTML = '<iframe src="data.meals["0"].strYoutube">';
     });
 }
+
+
+
