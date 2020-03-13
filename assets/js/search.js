016ecca3fb8c0ba7185dc882084ebf340d0df9c0
Jude <storeyjude@hotmail.com>
2020-03-13 21:47:17 +0000

    embeded youtube link from the object array

--- assets/js/search.js
+++ assets/js/search.js
@@ -14,9 +14,7 @@ function getData(type, cb) {
 }
 
 
-
-function writeToDocument(type) {
-           
+function writeToDocument(type) {           
 
     getData(type, function (data) {
         //div with id=data printing this//
@@ -24,51 +22,47 @@ function writeToDocument(type) {
         //food category breakfast lunch diner desert etc
         document.getElementById("recipe-category").innerHTML = data.meals["0"].strCategory;
 
-
         //link needs converted to image instead of showing a website link/ or broken image
         document.getElementById("recipe-image").innerHTML = `<img src=${data.meals["0"].strMealThumb}>`;
 
-
-
-        //document.getElementById("recipe-image").innerHTML = '<img src="data.meals["0"].strMealThumb">';
         //need list of ingredients an measures 1-20 on a for loop? Unpacking Our Data Onto The DOM lesson: 
         //checks if the meal has that corresponding ingredient-measure pair. If it does, we're putting it into the ingredients array. 
         //If there aren't any more ingredients we're stopping the for loop with a break condition. needs figured out next.
+        document.getElementById("recipe-ingred").innerHTML = data.meals["0"].strIngredient1; 
+        document.getElementById("recipe-ingred").innerHTML = data.meals["0"].strMeasure1; 
 
+        
         // description of how to carry out the meal
         document.getElementById("recipe-instruction").innerHTML = data.meals["0"].strInstructions;
 
-        let watchURL = data.meals["0"].strYoutube // console saing watchURL not defined at writeToDocument; at HTMLButtonElement.conclick
+        let watchURL = `${data.meals["0"].strYoutube}` // console saing watchURL not defined at writeToDocument; at HTMLButtonElement.conclick
         let newURL = watchURL.toString()  
-
-        //youtube clip needs embeeded as video instead of website link/ broken link / trying to use replace method
         
+        //youtube clip needs embeeded as video instead of website link/ broken link / trying to use replace method
+
         document.getElementById("recipe-vid").innerHTML = `<iframe src=${newURL}>`;
-
                       
-         function myFunction(){
+         
             var str = document.getElementById("recipe-vid").innerHTML;
-            var res = str.replace("watch", "embed");
+            
+            var res = str.replace("watch?","embed/");
+            
             document.getElementById("recipe-vid").innerHTML = res;
-        }
-
+            
+        
+            
                 
 
     });
        
-         
-
-        console.log(watchURL)
+       
 }
 
 
 
 
-
-        
-
 //  '<iframe src="data.meals["0"].strYoutube">'; 
-// document.getElementById("recipe-vid").innerHTML = '<iframe src="https://www.youtube.com/embed/${"data.meals["0"].strYoutube.slice(-11)}">';
+// document.getElementById("recipe-vid").innerHTML = '<iframe src="https://www.youtube.com/embed/$"{data.meals["0"].strYoutube.slice(-11)}">'; 
 
 
 
