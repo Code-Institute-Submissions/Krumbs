3b003b725b2e8292c726e8dc0e72c962b4f81bc6
Jude <storeyjude@hotmail.com>
2020-03-13 23:38:50 +0000

    code tidy up

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,13 +8,13 @@ function getData(type, cb) {
             cb(JSON.parse(this.responseText));
         }
     };
-    
+
     xhr.open("GET", baseURL + type + "/");
     xhr.send();
 }
 
 
-function writeToDocument(type) {           
+function writeToDocument(type) {
 
     getData(type, function (data) {
         //div with id=data printing this//
@@ -28,45 +28,26 @@ function writeToDocument(type) {
         //need list of ingredients an measures 1-20 on a for loop? Unpacking Our Data Onto The DOM lesson: 
         //checks if the meal has that corresponding ingredient-measure pair. If it does, we're putting it into the ingredients array. 
         //If there aren't any more ingredients we're stopping the for loop with a break condition. needs figured out next.
-        document.getElementById("recipe-ingred").innerHTML = data.meals["0"].strIngredient1; 
 
-        //for (let i = 1; i <= 20; i++) {
-	   // console.log(ingred[i]);
-       // }
+    
 
+        // document.getElementById("recipe-ingred").innerHTML = data.meals["0"].strIngredient1; 
         //document.getElementById("recipe-meas").innerHTML = data.meals["0"].strMeasure1; 
 
-        
+
         // description of how to carry out the meal
         document.getElementById("recipe-instruction").innerHTML = data.meals["0"].strInstructions;
 
 
-
         //youtube clip embeded
-        
+
         let watchURL = `${data.meals["0"].strYoutube}`
-        let newURL = watchURL.toString()  
-        console.log(res)
-                
+        let newURL = watchURL.toString()
 
-        var str = newURL;  
-        var res = str.replace("watch?","embed/");   
-        document.getElementById("recipe-vid").innerHTML = `<iframe src=${res}>`;   
-        console.log(res)
-        //document.getElementById("recipe-vid").innerHTML = `<iframe src=${newURL}>`; `<iframe src=${res}>`;
-         
+
+        var str = newURL;
+        var res = str.replace("watch?", "embed/");
+        document.getElementById("recipe-vid").innerHTML = `<iframe src=${res}>`;
+       
     });
-       
-            
-        
-       
 }
-
-
-
-
-//  '<iframe src="data.meals["0"].strYoutube">'; 
-// document.getElementById("recipe-vid").innerHTML = '<iframe src="https://www.youtube.com/embed/$"{data.meals["0"].strYoutube.slice(-11)}">'; 
-
-
-
