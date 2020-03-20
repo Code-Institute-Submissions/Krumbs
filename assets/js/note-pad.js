73f4b5d767aaf4d3685d5068062994b6ff9b991f
Jude <storeyjude@hotmail.com>
2020-03-20 14:51:51 +0000

    can add new item to list but not saving yet

--- assets/js/note-pad.js
+++ assets/js/note-pad.js
@@ -1,17 +1,30 @@
 const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
 
-let lists = [{
-    id: 1,
-    name: 'name'
-}, {
-    id: 2,
-    name: 'todo'
-}]
+let lists = []
+// allows adding new item to My list
+newListForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const listName = newListInput.value
+  if (listName == null || listName === '') return
+  const list = createList(listName)
+  //clear input when item added
+  newListInput.value = null
+  lists.push(list)
+  render()
+})
 
+function createList(name) {
+  return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+// set up my list so new item can be added on to end
 function render() {
     clearElement(listsContainer)
     lists.forEach(list => {
         const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
         listElement.classList.add(list-name)
         listElement.innerHTML = list.name
         listsContainer.appendChild(listElement)
