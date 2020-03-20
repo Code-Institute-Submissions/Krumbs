eafcac8d8c1a29c3cc4a09db6042a017ada6761c
Jude <storeyjude@hotmail.com>
2020-03-20 14:39:15 +0000

    comment out code

--- assets/js/note-pad.js
+++ assets/js/note-pad.js
@@ -1,3 +1,27 @@
 const listsContainer = document.querySelector('[data-lists]')
 
-let lists = []
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'todo'
+}]
+
+function render() {
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.classList.add(list-name)
+        listElement.innerHTML = list
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
